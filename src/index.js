98d1feec00b4cde78f66b5e2717a10ee18fc24fd
Tomoya Uetsuji <uetsuji.tomoya@gmail.com>
2015-10-23 09:48:14 +0900

    ranges were shourtened.

--- src/index.js
+++ src/index.js
@@ -48,7 +48,7 @@ document.getElementById('load-button').addEventListener('click', function () {
               j=0; //何個目の集計単位か。
               var karabasho;
               var reason;
-              var kara="から"
+              var kara="て"
               while(i<path.length){
                 tegakari=0;//手がかりがあるか
                 keitaisokaiseki[j] = new Array; //一文
@@ -64,14 +64,11 @@ document.getElementById('load-button').addEventListener('click', function () {
                     while(i>0){
                       i=i-1;
                       if(path[i].basic_form=="。"||path[i].basic_form=="？"||path[i].basic_form=="?"||path[i].basic_form=="、"){
-                        if(toutencount==0){
-                          toutencount=1;
-                          continue;
-                        } else{
+
                           buntou=i;
                           toutencount=0;
                           break;
-                        }
+
                       }
                     }
                     if(i==0){
