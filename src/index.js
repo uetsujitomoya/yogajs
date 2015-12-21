b28d5d471c64b0f08626694b66fea1f18277266e
Tomoya Uetsuji <uetsuji.tomoya@gmail.com>
2015-12-21 17:55:30 +0900

    data -> stackdata

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "kuromoji"
 var svg = d3.select("body").append("svg")
             .attr("width",200)
             .attr("height",200);
-        var dataArr = [
+        var stackdataArr = [
             [{x:1,y:3},{x:2,y:5},{x:3,y:7},{x:4,y:3},{x:5,y:5},{x:6,y:7}],
             [{x:1,y:2},{x:2,y:4},{x:3,y:9},{x:4,y:7},{x:5,y:6},{x:6,y:4}],
             [{x:1,y:1},{x:2,y:8},{x:3,y:5},{x:4,y:3},{x:5,y:5},{x:6,y:7}],
@@ -16,8 +16,8 @@ var svg = d3.select("body").append("svg")
             .x(function(d){return 1;})
             .y(function(d){return d.y;})
             .values(function(d){return d;});
-        var data = stack(dataArr);
-        var max = d3.max(data[data.length - 1], function(d){return d.y + d.y0;});
+        var stackdata = stack(stackdataArr);
+        var max = d3.max(stackdata[stackdata.length - 1], function(d){return d.y + d.y0;});
         var scaleX = d3.scale.linear().domain([0,6]).range([0,200]);
         var scaleY = d3.scale.linear().domain([0,max]).range([0,200]);
         var colors = ["blue","purple","red","orange","yellow","#0f0","green"];
@@ -26,9 +26,8 @@ var svg = d3.select("body").append("svg")
             .y0(function(d){return 200})
             .y1(function(d){return 200 - scaleY(d.y+d.y0)});
         svg.selectAll("path")
-            .data(data.reverse())
+            .data(stackdata.reverse())
             .enter()
             .append("path")
             .attr("d", area)
             .attr("fill",function(d,i){return colors[i]});
-    
