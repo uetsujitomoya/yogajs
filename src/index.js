59f8aed20491902d066f828d3454d7b310b5bc94
Tomoya Uetsuji <uetsuji.tomoya@gmail.com>
2015-12-21 18:05:10 +0900

    add stack rayout(not linked )

--- src/index.js
+++ src/index.js
@@ -318,6 +318,8 @@ document.getElementById('load-button').addEventListener('click', function () {
   [100, 54, 255]
   ];
 
+
+
   // Euclidean distance
   function distance(a, b) {
   var d = 0;
@@ -350,3 +352,33 @@ document.getElementById('load-button').addEventListener('click', function () {
   console.log(clusters);
   // => [ [ [ 250, 255, 253 ] ],
   // => [ [ 100, 54, 255 ], [ 22, 22, 90 ], [ 20, 20, 80 ] ] ]
+
+
+
+
+
+          var stackdataArr = [
+              [{x:1,y:3},{x:2,y:5},{x:3,y:7},{x:4,y:3},{x:5,y:5},{x:6,y:7}],
+              [{x:1,y:2},{x:2,y:4},{x:3,y:9},{x:4,y:7},{x:5,y:6},{x:6,y:4}],
+              [{x:1,y:1},{x:2,y:8},{x:3,y:5},{x:4,y:3},{x:5,y:5},{x:6,y:7}],
+              [{x:1,y:8},{x:2,y:6},{x:3,y:3},{x:4,y:2},{x:5,y:7},{x:6,y:4}]
+          ];
+          var stack = d3.layout.stack()
+              .x(function(d){return 1;})
+              .y(function(d){return d.y;})
+              .values(function(d){return d;});
+          var stackdata = stack(stackdataArr);
+          var max = d3.max(stackdata[stackdata.length - 1], function(d){return d.y + d.y0;});
+          var scaleX = d3.scale.linear().domain([0,6]).range([0,200]);
+          var scaleY = d3.scale.linear().domain([0,max]).range([0,200]);
+          var colors = ["blue","purple","red","orange","yellow","#0f0","green"];
+          var area = d3.svg.area()
+              .x(function(d,i){return i * 200/5})
+              .y0(function(d){return 200})
+              .y1(function(d){return 200 - scaleY(d.y+d.y0)});
+          svg.selectAll("path")
+              .data(stackdata.reverse())
+              .enter()
+              .append("path")
+              .attr("d", area)
+              .attr("fill",function(d,i){return colors[i]});
