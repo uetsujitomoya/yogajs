9ce86823370c225ac1db8ccfbf5526acaa598b8a
Tomoya Uetsuji <uetsuji.tomoya@gmail.com>
2016-01-05 12:30:25 +0900

    naknadetakedoyappachouhukushiteru

--- src/index.js
+++ src/index.js
@@ -256,90 +256,90 @@ document.getElementById('load-button').addEventListener('click', function () {
 						if(RGB[m][i][0]+RGB[m][i][1]+RGB[m][i][2]>=1){
 						
 							console.log(hinshi[m][i][j]);
+							if(RGB[m][i][0]+RGB[m][i][1]+RGB[m][i][2]>=1){
+								
+									
+								
+									
+								if(RGB[m][i][0]==1){
+									//if(checkboxlist[k][1]==0){単語とグループの組み合わせの重複を防ぐ
+										target.innerHTML += "<input id=\"ken" + c + "\" type=checkbox /><label for="+c+">「" + keitaisokaiseki[m][i][j] + "」を「愛(恋愛･家族関係)」に。</label><br />";
+										console.log("%d,%d,%d,「%s」を「愛」に",m,i,j,keitaisokaiseki[m][i][j]);
+										
+										//checkboxlist[k][0]=1;
+										//checkboxlist[k][1]=1;
+										chboxlist[c]=new Array(2);
+										chboxlist[c][0]=keitaisokaiseki[m][i][j];
+										console.log("c=%d",c);
+										chboxlist[c][1]=0;
+										c++;
+									//}
+								}
+
+									
+								if(RGB[m][i][1]==1){
+									//if(checkboxlist[k][2]==0){
+										target.innerHTML += "<input id=\"ken" + c + "\" type=checkbox /><label for="+c+">「" + keitaisokaiseki[m][i][j] + "」を「交友関係」に。</label><br />";
+										console.log("%d,%d,%d,「%s」を「交友」に",m,i,j,keitaisokaiseki[m][i][j]);
+										
+										//checkboxlist[k][0]=1;
+										//checkboxlist[k][2]=1;
+										chboxlist[c]=new Array(2);
+										chboxlist[c][0]=keitaisokaiseki[m][i][j];
+										console.log("c=%d",c);
+										chboxlist[c][1]=1;
+										c++;
+									//}
+								}
+									
+								if(RGB[m][i][2]==1){
+									//if(checkboxlist[k][3]==0){
+										target.innerHTML += "<input id=\"ken" + c + "\" type=checkbox /><label for="+c+">「" + keitaisokaiseki[m][i][j] + "」を「仕事関係」に。</label><br />";
+										console.log("%d,%d,%d,「%s」を「仕事」に",m,i,j,keitaisokaiseki[m][i][j]);
+										
+										//checkboxlist[k][0]=1;
+										//checkboxlist[k][3]=1;
+										chboxlist[c]=new Array(2);
+										chboxlist[c][0]=keitaisokaiseki[m][i][j];
+										console.log("c=%d",c);
+										chboxlist[c][1]=2;
+										c++;
+										
+									//}
+									
+								}
+
+								
+								
+
+								//checkboxを出す
+								//<input id=Checkbox1 type=checkbox /><label for=Checkbox1>チェック項目1</label><br />
+								
+								
+								
+								
+								
+
+								
+
+								
+
+								
+								//関連発言内の全単語を出力する
+
+
+
+							}
+                    
+
 						}
                   
+
 
 						for(k=0;k<miserables.nodes.length;++k){
 
 							if(keitaisokaiseki[m][i][j]==miserables.nodes[k].name){
 								list[m][i][k]=1;
-								if(RGB[m][i][0]+RGB[m][i][1]+RGB[m][i][2]>=1){
-								
-									
-								
-									
-									if(RGB[m][i][0]==1){
-										if(checkboxlist[k][1]==0){//単語とグループの組み合わせの重複を防ぐ
-											target.innerHTML += "<input id=\"ken" + c + "\" type=checkbox /><label for="+c+">「" + miserables.nodes[k].name + "」を「愛(恋愛･家族関係)」に。</label><br />";
-											console.log("%d,%d,%d,「%s」を「愛」に",m,i,j,miserables.nodes[k].name);
-										
-											checkboxlist[k][0]=1;
-											checkboxlist[k][1]=1;
-											chboxlist[c]=new Array(2);
-											chboxlist[c][0]=miserables.nodes[k].name;
-											console.log("c=%d,k=%d",c,k);
-											chboxlist[c][1]=0;
-											c++;
-										}
-									}
-
-									
-									if(RGB[m][i][1]==1){
-										if(checkboxlist[k][2]==0){
-											target.innerHTML += "<input id=\"ken" + c + "\" type=checkbox /><label for="+c+">「" + miserables.nodes[k].name + "」を「交友関係」に。</label><br />";
-											console.log("%d,%d,%d,「%s」を「交友」に",m,i,j,miserables.nodes[k].name);
-										
-											checkboxlist[k][0]=1;
-											checkboxlist[k][2]=1;
-											chboxlist[c]=new Array(2);
-											chboxlist[c][0]=miserables.nodes[k].name;
-											console.log("c=%d,k=%d",c,k);
-											chboxlist[c][1]=1;
-											c++;
-										}
-									}
-									
-									if(RGB[m][i][2]==1){
-										if(checkboxlist[k][3]==0){
-											target.innerHTML += "<input id=\"ken" + c + "\" type=checkbox /><label for="+c+">「" + miserables.nodes[k].name + "」を「仕事関係」に。</label><br />";
-											console.log("%d,%d,%d,「%s」を「仕事」に",m,i,j,miserables.nodes[k].name);
-										
-											checkboxlist[k][0]=1;
-											checkboxlist[k][3]=1;
-											chboxlist[c]=new Array(2);
-											chboxlist[c][0]=miserables.nodes[k].name;
-											console.log("c=%d,k=%d",c,k);
-											chboxlist[c][1]=2;
-											c++;
-										
-										}
-									
-									}
-
-								
-								
-
-									//checkboxを出す
-									//<input id=Checkbox1 type=checkbox /><label for=Checkbox1>チェック項目1</label><br />
-								
-								
-								
-								
-								
-
-								
-
-								
-
-								
-									//関連発言内の全単語を出力する
-
-
-
-								}
-                    
-
-
 							}
 
 							//checkbox用
