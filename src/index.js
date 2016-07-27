9513df72ff9656da4e329866ab84420fd25657fe
上辻智也 <uetsuji@viz.media.kyoto-u.ac.jp>
2016-07-27 17:58:53 +0900

    remove []

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-//import {setForViz} from "./svg.js";
 import {funcReaderOnload} from "./wordparse.js";
-//import $ from 'jquery';
+import $ from 'jquery';
 var keitaisokaiseki = []; //このlengthは段落数
 var RGBlist  = [];//checkboxのセレクト結果
 var chboxlist=[];//通し番号
@@ -23,16 +22,57 @@ document.getElementById('load-button').addEventListener('click', function () {
 });
 document.getElementById('storageSave-button').addEventListener('click', function () {
 
-  var data=localStorage;
-
+  let file_name="storage"+Date()+".csv";
 
   var ary = window.localStorage; //http://hakuhin.jp/js/storage.html#STORAGE_00
-  console.log("ary");
-  console.log(ary);
-  var blob = new Blob([ary], {type: "text/csv"});
-  var url = URL.createObjectURL(blob);
-  var a = document.querySelector("#results"); // id of the <a> element to render the download link
-  a.href = url;
-  a.download = "file.csv";
+
+  //let csv_string=convertTocsv(ary);
+
+  //UTF-16に変換...(1)
+  /*var array = [];
+  for (var i=0; i<csv_string.length; i++){
+    array.push(csv_string.charCodeAt(i));
+  *}*/
+  //var csv_contents = new Uint16Array(array);
+
+  //ファイル作成
+  var blob = new Blob([ary] , {
+    type: "text/plain"
+  });
+
+  //ダウンロード実行...(2)
+  if (window.navigator.msSaveOrOpenBlob) {
+    //IEの場合
+    navigator.msSaveBlob(blob, file_name);
+  } else {
+    //IE以外(Chrome, Firefox)
+    var downloadLink = $('<a></a>');
+    downloadLink.attr('href', window.URL.createObjectURL(blob));
+    downloadLink.attr('download', file_name);
+    downloadLink.attr('target', '_blank');
+
+    $('body').append(downloadLink);
+    downloadLink[0].click();
+    downloadLink.remove();
+  }
 
 });
+
+//http://qiita.com/kenchan0130/items/11c3abab109405379ffb
+/*var convertTocsv = function(objArray) {
+  //if (!(objArray[0] instanceof Array)) return objArray.join(',');
+  var array = (typeof(objArray) != 'object') ? JSON.parse(objArray) : objArray;
+  var str = '';
+
+  for (var i = 0; i < array.length; i++) {
+    var line = '';
+    for (var index in array[i]) {
+      if (line !== '') line += ',';
+      line += '"' + array[i][index] + '"';
+  *  }
+    str += line + '\r\n';
+  *}
+
+  return str;
+*};
+*/
