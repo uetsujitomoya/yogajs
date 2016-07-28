dcac3e8e293f71c16b55c00008dd80e15ae8dbb2
上辻智也 <uetsuji@viz.media.kyoto-u.ac.jp>
2016-07-28 14:23:39 +0900

    多分localStorageをCSVで吐き出せる

--- src/index.js
+++ src/index.js
@@ -26,21 +26,80 @@ document.getElementById('storageSave-button').addEventListener('click', function
 
   var ary = window.localStorage; //http://hakuhin.jp/js/storage.html#STORAGE_00
 
-  //let csv_string=convertTocsv(ary);
 
-  //UTF-16に変換...(1)
-  /*var array = [];
-  for (var i=0; i<csv_string.length; i++){
-    array.push(csv_string.charCodeAt(i));
-  *}*/
-  //var csv_contents = new Uint16Array(array);
+  // ウェブストレージに対応している http://hakuhin.jp/js/storage.html#STORAGE_GET_KEYS
+  if(window.localStorage){
+
+
+
+    // ------------------------------------------------------------
+    // キーの総数を取得する
+    // ------------------------------------------------------------
+    var num = window.sessionStorage.length;
+
+    // ------------------------------------------------------------
+    // ストレージからすべてのキーを取得する
+    // ------------------------------------------------------------
+    var i;
+    let csv_array=[];
+    for(i=0;i< num;i++){
+      csv_array[i]=[];
+
+      // 位置を指定して、ストレージからキーを取得する
+      csv_array[i][0] = window.localStorage.key(i);
+
+      // ストレージからデータを取得する
+
+      csv_array[i][1] = window.localStorage.getItem(csv_array[i][0]);
+
+      // 出力テスト
+      console.log("name:" + csv_array[i][0] + " value:" + csv_array[i][1]);
+    }
+
+  }
+
+  //CSVに記載するデータ配列
+
+  var file_name = 'test.csv';
+
+  //配列をTAB区切り文字列に変換
+  var csv_string = "";
+  for (var i=0; i<csv_array.length; i++) {
+    csv_string += csv_array[i].join("\t");
+    csv_string += '\r\n';
+  }
+
+  //BOM追加
+  csv_string = "\ufffe" + csv_string; //UTF-16
+  console.log (csv_string);
+
+  //実行環境がLEかどうか判別...(3)
+  if (isLittleEndian()) {
+
+    //実行環境のエンディアンがLEならTypedArrayを利用
+    var array = [];
+    for (var i=0; i<csv_string.length; i++){
+      array.push(csv_string.charCodeAt(i));
+    }
+    var csv_contents = new Uint16Array(array);
+
+  } else {
+
+    //LEでない場合はDataViewでUTF-16LEのArrayBufferを作成
+    var array_buffer = new ArrayBuffer(csv_string.length * 2);
+    var data_view = new DataView(array_buffer);
+    for (var i=0,j=0; i<csv_string.length; i++,j=i*2) {
+      data_view.setUint16( j, csv_string.charCodeAt(i), true ); //第3引数にtrueを渡すとLEになる
+    }
+    var csv_contents = array_buffer
+  }
 
   //ファイル作成
-  var blob = new Blob([ary] , {
-    type: "text/plain"
+  var blob = new Blob([csv_contents] , {
+    type: "text/csv;charset=utf-16;"
   });
 
-  //ダウンロード実行...(2)
+  //ダウンロード実行
   if (window.navigator.msSaveOrOpenBlob) {
     //IEの場合
     navigator.msSaveBlob(blob, file_name);
@@ -56,23 +115,18 @@ document.getElementById('storageSave-button').addEventListener('click', function
     downloadLink.remove();
   }
 
-});
+  // --------------------------------------
+  // 実行環境のエンディアンがLEかどうか判別
+  // --------------------------------------
+  function isLittleEndian(){
+    if ((new Uint8Array((new Uint16Array([0x00ff])).buffer))[0]) return true;
+    return false;
+  }
+}
+
+//let csv_string=convertTocsv(ary);
+
+
+
 
 //http://qiita.com/kenchan0130/items/11c3abab109405379ffb
-/*var convertTocsv = function(objArray) {
-  //if (!(objArray[0] instanceof Array)) return objArray.join(',');
-  var array = (typeof(objArray) != 'object') ? JSON.parse(objArray) : objArray;
-  var str = '';
-
-  for (var i = 0; i < array.length; i++) {
-    var line = '';
-    for (var index in array[i]) {
-      if (line !== '') line += ',';
-      line += '"' + array[i][index] + '"';
-  *  }
-    str += line + '\r\n';
-  *}
-
-  return str;
-*};
-*/
