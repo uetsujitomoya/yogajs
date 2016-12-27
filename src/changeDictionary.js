8ce9ae43e82ccd0c94a182262f0de452c4a981d5
上辻智也 <uetsuji@viz.media.kyoto-u.ac.jp>
2016-12-27 13:51:27 +0900

    csv2array

--- src/changeDictionary.js
+++ src/changeDictionary.js
@@ -27,6 +27,12 @@ var ReadDictionary = (c) =>{
 
     //ヒットしたら点数加えてブレイク
 
+    var data = csv2Array('HDFaceVertex.csv');
+    console.log(data);
+
+
+
+
 
     var testArray = [3, 8, 13, true, 'あいうえお', 8, 10];
 
@@ -47,3 +53,20 @@ var ReadDictionary = (c) =>{
 
 
 };
+
+
+function csv2Array(filePath) { //csvﾌｧｲﾙﾉ相対ﾊﾟｽor絶対ﾊﾟｽ
+    var csvData = [];
+    var data = new XMLHttpRequest();
+    data.open("GET", filePath, false); //true:非同期,false:同期
+    data.send(null);
+    var LF = String.fromCharCode(10); //改行ｺｰﾄﾞ
+    var lines = data.responseText.split(LF);
+    for (var i = 0; i < lines.length;++i) {
+        var cells = lines[i].split(",");
+        if( cells.length != 1 ) {
+            csvData.push(cells);
+        }
+    }
+    return csvData;
+}
