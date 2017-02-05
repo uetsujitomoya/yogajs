73f6a61aaa7753902cf10cbed9ee248e32a61cb5
上辻智也 <uetsuji@viz.media.kyoto-u.ac.jp>
2017-02-06 01:31:55 +0900

    ConvertForKNP

---
+++ src/ConvertForKNP.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+f = open('../160402.txt','r')
+Allf = f.read()
+
+text = Allf.replace('¡£','¡£\r\n')
+#print text,
+
+txtForWrite = open('../160402ForPutIntoKNP.txt','w')
+txtForWrite.write(text)
+txtForWrite.close()
+
+f.close()
