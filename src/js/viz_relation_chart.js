0560e8bd6d49ea97363bc0282c20fbf751b6230d
上辻智也 <uetsuji@viz.media.kyoto-u.ac.jp>
2017-08-13 01:54:02 +0900

    [WIP]add : fix the points of nodes and arrows

--- src/js/viz_relation_chart.js
+++ src/js/viz_relation_chart.js
@@ -7,17 +7,21 @@ let viz_relation_chart = () => {
     //viz_arrow();
 }
 
-let viz_circle = () => {
-    fix_circle_width();
+let viz_circle = (times,isClient,text) => {
+    fix_circle_width(times);
     fix_circle_radius();
-    fix_circle_color();
-    fix_circle_text();
+    fix_circle_color(isClient);
+    fix_circle_text(text);
+    fix_circle_x();
+    fix_circle_y();
 }
 
-let viz_arrow = () => {
-    fix_arrow_width();
+let viz_arrow = (times,isClient) => {
+    fix_arrow_width(times);
     fix_arrow_length();
-    fix_arrow_color();
+    fix_arrow_color(isClient);
+    fix_arrow_start();
+    fix_arrow_end();
 }
 
 export{viz_relation_chart}
