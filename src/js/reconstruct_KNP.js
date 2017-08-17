1f55ce7a9fe7b071577c9adf4a8da030a7c4c7d4
上辻智也 <uetsuji@viz.media.kyoto-u.ac.jp>
2017-08-17 18:31:15 +0900

    [WIP] add : find kakarareru bunsetsu & kihonku id

--- src/js/reconstruct_KNP.js
+++ src/js/reconstruct_KNP.js
@@ -17,6 +17,26 @@ let reconstruct_KNP = (raw_2d_array) => {
     })
 }
 
+let count_bunsetsu=(input_2d_array)=>{
+    let cnt=0
+    input_2d_array.forEach((row_array)=>{
+        if(row_array[0]=="*"){
+            cnt++
+        }
+    })
+    return cnt
+}
+
+let count_kihonku=(input_2d_array)=>{
+    let cnt=0
+    input_2d_array.forEach((row_array)=>{
+        if(row_array[0]=="+"){
+            cnt++
+        }
+    })
+    return cnt
+}
+
 //文節取得
 /*EODまでが一個の文
  アスタリスクが1個の文節 phtase_no
@@ -27,13 +47,21 @@ let reconstruct_KNP = (raw_2d_array) => {
 class KNP_sentence{
     constructor(raw_rowNo,raw_2d_array) {
 
+        //動詞判定・登場人物判定と組み合わせる
+
+        //PARAがかかる先は？
+
         //PARAの探索（3列目にPを含む文字列があればPARAがそこにあると判定し、文節配列と基本句配列にPARAを挿入）
         //そのためには、始めに文節配列・基本句配列のlengthだけ決めてしまったほうが良い
 
         //this.csv_raw_array=[]
         this.rowNo=raw_rowNo
         this.bunsetsu_array=[]
+        this.bunsetsu_array.length=count_bunsetsu(raw_2d_array)
+
         this.kihonku_array=[]
+        this.kihonku_array.length=count_kihonku(raw_2d_array)
+
         this.surface_form = "null"
         //this.basic_form = row_array[2]
 
@@ -45,30 +73,30 @@ class KNP_sentence{
         temp_2d_array_for_bunsetsu.push(raw_2d_array[2])//0基本句目　開始宣言をプッシュ
         temp_2d_array_for_kihonku.push(raw_2d_array[2])//0基本句目　開始宣言をプッシュ
 
-        let clause_No_in_sentence=0
-        let kihonku_No_in_sentence=0
+        let bunsetsu_num_in_sentence=0
+        let kihonku_num_in_sentence=0
 
         for(let temp_rowNo=2;temp_rowNo<raw_2d_array.length;temp_rowNo++){
             let temp_surface_form=raw_2d_array[temp_rowNo][0];
             if(temp_surface_form=="+" && raw_2d_array[temp_rowNo-1][0]!="*"){//文節内 2こ目以降の基本句
-                this.kihonku_array.push(new KNP_kihonku_in_sentence(temp_2d_array_for_kihonku))//文の中の通し番号での基本句array
+                this.kihonku_array[kihonku_num_in_sentence] = new KNP_kihonku_in_sentence(kihonku_num_in_sentence,temp_2d_array_for_kihonku)//文の中の通し番号での基本句array
                 temp_2d_array_for_kihonku=[]
-                kihonku_No_in_sentence++
+                kihonku_num_in_sentence++
             }else if( temp_surface_form == "*" ){
-                this.kihonku_array.push(new KNP_kihonku_in_sentence(temp_2d_array_for_kihonku))//文の中の通し番号での基本句array
+                this.kihonku_array[kihonku_num_in_sentence] = new KNP_kihonku_in_sentence(kihonku_num_in_sentence,temp_2d_array_for_kihonku)//文の中の通し番号での基本句array
                 temp_2d_array_for_kihonku=[]
-                kihonku_No_in_sentence=0
+                kihonku_num_in_sentence=0
 
-                this.bunsetsu_array.push(new KNP_bunsetsu(temp_2d_array_for_bunsetsu))//文の中の通し番号での基本句array
+                this.bunsetsu_array[bunsetsu_num_in_sentence] = new KNP_bunsetsu(bunsetsu_num_in_sentence,temp_2d_array_for_bunsetsu)//文の中の通し番号での文節array
                 temp_2d_array_for_bunsetsu=[]
-                clause_No_in_sentence++
+                bunsetsu_num_in_sentence++
             }
             temp_2d_array_for_bunsetsu.push(raw_2d_array[temp_rowNo])
             temp_2d_array_for_kihonku.push(raw_2d_array[temp_rowNo])
         }
 
-        this.bunsetsu_array.push(new KNP_bunsetsu())
-        this.kihonku_array.push(new KNP_kihonku_in_sentence())//文の中の通し番号での基本句array
+        this.bunsetsu_array[bunsetsu_num_in_sentence] = new KNP_bunsetsu(bunsetsu_num_in_sentence,temp_2d_array_for_bunsetsu)
+        this.kihonku_array[kihonku_num_in_sentence] = new KNP_kihonku_in_sentence(kihonku_num_in_sentence,temp_2d_array_for_kihonku)//文の中の通し番号での基本句array
         /*
         raw_2d_array.forEach((row_array)=>{
             
@@ -76,29 +104,80 @@ class KNP_sentence{
         */
 
         //最後に、掛かり先を探索して埋める
-        this.input_each_kakarareru_id();
+        this.input_each_kakarareru_bunsetsu_id();
+        this.input_each_kakarareru_kihonku_id();
     }
 
-    input_each_kakarareru_id () {
+    input_each_kakarareru_bunsetsu_id () {
         //動詞なら、その動詞にかかるのを探していく
+        this.bunsetsu_array.forEach((kakaru_bunsetsu)=>{
+            for(let kakarareru_bunsetsu_num = 0 ; kakarareru_bunsetsu_num < this.bunsetsu_array.length ; kakarareru_bunsetsu_num++){
 
+                console.log(this.bunsetsu_array[kakarareru_bunsetsu_num])
+                if(kakaru_bunsetsu.kakaru_bunsetsu_id == this.bunsetsu_array[kakarareru_bunsetsu_num].id){
+                    this.bunsetsu_array[kakarareru_bunsetsu_num].kakarareru_bunsetsu_id_array.push(kakaru_bunsetsu.id)
+                    break;
+                }
+            }
+        })
+    }
+
+    input_each_kakarareru_kihonku_id(){
+        this.kihonku_array.forEach((kakaru_kihonku)=>{
+
+            for(let kakarareru_kihonku_num = 0 ; kakarareru_kihonku_num < this.kihonku_array.length ; kakarareru_kihonku_num++){
+                console.log(this.kihonku_array[kakarareru_kihonku_num])
+                if(kakaru_kihonku.kakatu_kihonku_id == this.kihonku_array[kakarareru_kihonku_num].id){
+                    this.kihonku_array[kakarareru_kihonku_num].kakarareru_kihonku_id_array.push(kakaru_kihonku.id)
+                }
+            }
+        })
     }
 }
 
 class KNP_bunsetsu {
     constructor(num,input_2d_array) {
+        console.log(input_2d_array)
 
         this.csv_raw_array=input_2d_array
-        this.rowNo=rowNo
+        //this.rowNo=rowNo
         this.id = num+"D"
         this.kihonku_array=[]
         this.word_array=[]
+
+        /*
         if(this.word_array[0][3]=="動詞"){
             this.verb_data={}
         }
+        */
         this.kakaru_bunsetsu_id = input_2d_array[0][1]
-        this.kakarareru_bunsetsu_id = "null"
-        this.surface_form = row_array[0]
+        this.kakarareru_bunsetsu_id_array = []
+        this.surface_form = ""
+        input_2d_array.forEach((row_array)=>{
+            this.surface_form += row_array[0]
+        })
+    }
+}
+
+class KNP_kihonku_in_sentence {
+    constructor(num,input_2d_array) {
+        console.log(input_2d_array)
+
+        this.csv_raw_array=input_2d_array
+        //this.rowNo=rowNo
+        this.id = num+"D"
+        this.word_array=[]
+        /*
+        if(this.word_array[0][3]=="動詞"){
+            this.verb_data={}
+        }
+        */
+        this.kakaru_kihonku_id = input_2d_array[0][1]
+        this.kakarareru_kihonku_id_array = []
+        this.surface_form = ""
+        input_2d_array.forEach((row_array)=>{
+            this.surface_form += row_array[0]
+        })
     }
 }
 
@@ -121,24 +200,7 @@ class KNP_kihonku_in_clause {
     }
 }
 
-class KNP_kihonku_in_sentence {
-    constructor(rowNo,row_array,upper_row_array) {
 
-        this.csv_raw_array=[]
-        this.rowNo=rowNo
-        this.num = num+"D"
-        this.word_array=[]
-        if(this.word_array[0][3]=="動詞"){
-            this.verb_data={}
-        }
-
-        this.subject = "null"
-        this.object = "null"
-        this.kakarareruNo = upper_row_array[1]
-        this.kakaruNo = "null"
-        this.surface_form = row_array[0]
-    }
-}
 
 class KNP_word {
     constructor(rowNo,row_array,upper_row_array) {
