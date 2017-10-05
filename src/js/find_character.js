bca011f02f4852cbff2db1e7cb11ef7ed9d4fe71
上辻智也 <uetsuji@viz.media.kyoto-u.ac.jp>
2017-10-05 14:48:28 +0900

    fix: remove error, but not "Aさん".

--- src/js/find_character.js
+++ src/js/find_character.js
@@ -9,126 +9,126 @@ const characterKeyword="カテゴリ:人>"
 //Aさんを追加→1単語とする
 
 let find_character = (knparray,KNP_character_array) => {
-    //if かな exist
+  //if かな exist
 
-    for(const row of knparray){//"さん"判断にindex使いたいからアカンわ。
-        if(contains_japanese(row[0])){
-            let temp_japanese = row[0]
-            for(const element of row){
-                if ( element.match(characterKeyword)) {
-                    if(isNewCharacter()){
-                        createNewCharacter()
-                    }
-                }
-            }
-            //さんの場合、1つ前遡って足して、Aさん
-            if(row[0]==="さん"){
-                if(isNewCharacter()){
-                    createNewCharacter()
-                }
-            }
+  /*
+  for(const row of knparray){//"さん"判断にindex使いたいからアカンわ。
+    if(contains_japanese(row[0])){
+      let temp_japanese = row[0]
+      for(const element of row){
+        if ( element.match(characterKeyword)) {
+          if(isNewCharacter()){
+            createNewCharacter()
+          }
         }
+      }
+      //さんの場合、1つ前遡って足して、Aさん
+      if(row[0]==="さん"){
+        if(isNewCharacter()){
+          createNewCharacter()
+        }
+      }
     }
+  }*/
 
 
 
-    knparray.forEach((row)=>{
-        //console.log(row)
-        if(contains_japanese(row[0])){
-            let temp_japanese = row[0]
-            for(const element of row){
-                if ( element.match(characterKeyword)) {
-                    let temp_character_name=row[0];
-                    let isNewCharacter = true
-                    //被ってなければその登場人物のインスタントをつくる
-                    KNP_character_array.forEach((character)=>{
-                        if(temp_character_name===character.name){
-                            //verbを追加
-                            isNewCharacter = false;
-                            //太さ加算
-                            //character.character_node.bold_qty++
-                        }
-                    })
-                    if(isNewCharacter === true){
-                        KNP_character_array.push(new KNP_character(temp_character_name))
-                    }
-                }
+  knparray.forEach((row,index)=>{
+    //console.log(row)
+    if(contains_japanese(row[0])){
+      let temp_japanese = row[0]
+      for(const element of row){
+        if ( element.match(characterKeyword)) {
+          let temp_character_name=row[0];
+          let isNewCharacter = true
+          //被ってなければその登場人物のインスタントをつくる
+          KNP_character_array.forEach((character)=>{
+            if(temp_character_name===character.name){
+              //verbを追加
+              isNewCharacter = false;
+              //太さ加算
+              //character.character_node.bold_qty++
             }
-            /*
-             row.forEach((element)=>{
-             if ( element.match(characterKeyword)) {
-             let temp_character_name=row[0];
-             let isNewCharacter = true
-             //被ってなければその登場人物のインスタントをつくる
-             KNP_character_array.forEach((character)=>{
-             if(temp_character_name==character.name){
-             //verbを追加
-             isNewCharacter = false;
-             //太さ加算
-             //character.character_node.bold_qty++
-             }
-             })
-             if(isNewCharacter==true){
-             KNP_character_array.push(new KNP_character(temp_character_name))
-             }
-             }
-             })
-             */
-            //さんの場合、1つ前遡って足して、Aさん
-            if(row[0] === "さん"){
-                const tempCharacter = knparray[index-1][0]+row[0]
-                if(isNewCharacter(tempCharacter)){
-                    createNewCharacter(tempCharacter)
-                }
-            }
+          })
+          if(isNewCharacter === true){
+            KNP_character_array.push(new KNP_character(temp_character_name))
+          }
         }
-    })
+      }
+      /*
+       row.forEach((element)=>{
+       if ( element.match(characterKeyword)) {
+       let temp_character_name=row[0];
+       let isNewCharacter = true
+       //被ってなければその登場人物のインスタントをつくる
+       KNP_character_array.forEach((character)=>{
+       if(temp_character_name==character.name){
+       //verbを追加
+       isNewCharacter = false;
+       //太さ加算
+       //character.character_node.bold_qty++
+       }
+       })
+       if(isNewCharacter==true){
+       KNP_character_array.push(new KNP_character(temp_character_name))
+       }
+       }
+       })
+       */
+      //さんの場合、1つ前遡って足して、Aさん
+      if(row[0] === "さん"){
+        const tempCharacter = knparray[index-1][0]+row[0]
+        if(isNewCharacter(tempCharacter,KNP_character_array)){
+          createNewCharacter(tempCharacter)
+        }
+      }
+    }
+  })
 }
 
 let isNewCharacter=(tempCharacterName,characterArray)=> {
-    for(const character of characterArray){
-        if(tempCharacterName === character.name){
-            //verbを追加
-            return false;
-
-            //太さ加算
-            //character.character_node.bold_qty++
-        }
+  characterArray.forEach((character)=>{
+    if(tempCharacterName === character.name){
+      //verbを追加
+      return false;
+      //太さ加算
+      //character.character_node.bold_qty++
     }
-    return true
+  })
+  return true
 }
 
 let createNewCharacter=()=>{
-    //Nodeを出現させるとしたらどこまでつくるかも書く
+  //Nodeを出現させるとしたらどこまでつくるかも書く
 
 }
 
 class character{
-    constructor(){
-        this.chalacter_name="null"
-        this.isClient=false
-        this.character_node = new Node(character_name,sentence,this.isClient)
+  constructor(){
+    this.chalacter_name="null"
+    this.isClient=false
+    this.character_node = new Node(character_name,sentence,this.isClient)
 
-    }
+  }
 }
 
 
 
 class Node{
-    constructor(character_name,sentence,isClient){
-        //this.character=character
-        this.node_r=0
-        this.node_x=0
-        this.node_y=0
-        this.node_text=character_name
-        this.bold_qty=1
-        if(isClient){
-            this.node_color=color_of_client
-        }else{
-            this.node_color=color_of_people_around_client
-        }
-
+  constructor(character_name,sentence,isClient){
+    //this.character=character
+    this.node_r=0
+    this.node_x=0
+    this.node_y=0
+    this.node_text=character_name
+    this.bold_qty=1
+    if(isClient){
+      this.node_color=color_of_client
+    }else{
+      this.node_color=color_of_people_around_client
     }
+
+  }
 }
 
 
@@ -142,12 +142,12 @@ export {find_character}
 
 
 class KNP_character {
-    constructor(name) {
-        this.name = name
-        if(this.name === "私"){
-            this.client = 1
-        }else{
-            this.client = 0
-        }
+  constructor(name) {
+    this.name = name
+    if(this.name === "私"){
+      this.client = 1
+    }else{
+      this.client = 0
     }
+  }
 }
