06f214924df82412c878b8e01ad1719dc203618e
上辻智也 <uetsuji@viz.media.kyoto-u.ac.jp>
2017-10-30 13:46:16 +0900

    [WIP] add: applySliderForNodeText

---
+++ src/js/charaChart/viz/applySliderForNodeText.js
@@ -0,0 +1,15 @@
+const applySliderForNodeText=(sliderBunArr,nodeArr)=>{
+  for(let node of nodeArr){
+    node.bunArr=[]
+    for(const bun of sliderBunArr){
+      if(bun.charaNameArr.indexOf(node.name) >= 0){
+        node.bunArr+={
+          idx: bun.idx,
+          surfaceForm: bun.surfaceForm
+        }
+      }
+    }
+  }
+}
+
+export {applySliderForNodeText}
