d308a6cb3c7660a4e62996bb737676dcf1447b68
上辻智也 <uetsuji@viz.media.kyoto-u.ac.jp>
2017-10-30 13:33:25 +0900

    layout chosei(not apply slider value for textView)

--- src/js/charaChart/viz/viewText.js
+++ src/js/charaChart/viz/viewText.js
@@ -35,9 +35,8 @@ const createTextViewArea = (bun,allBunArr,i)=>{
 
 const createBox=(i)=>{
   //箱を作る
-
   let msg = document.getElementById('box')
-  msg.innerHTML += '<div id="msg' + i +'" style="border-style: solid; border-width: 1px; border-color:' + rodata.boxBorderColor + '; height:300px; width:600px; overflow-y:scroll; margin-bottom: 5px; padding-bottom: 5px; padding-top: 5px;">'
+  msg.innerHTML += '<div id="msg' + i +'" style="border-width: 1px; background-color:#ffffff; width:600px; overflow-y:scroll; margin-bottom: 5px; margin-left: 5px; margin-top: 10px; padding-bottom: 5px; padding-top: 5px;">'
 
 }
 
