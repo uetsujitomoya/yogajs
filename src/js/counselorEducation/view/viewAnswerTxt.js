120268c6e2f99e4b61bb642f75d36ec3ba06daa6
上辻智也 <uetsuji@viz.media.kyoto-u.ac.jp>
2017-12-27 11:39:21 +0900

    add: successfully visualization only txt.

--- src/js/counselorEducation/view/viewAnswerTxt.js
+++ src/js/counselorEducation/view/viewAnswerTxt.js
@@ -5,22 +5,22 @@ const txtViewClient=rodata.txtView.client
 
 const viewAnswerTxt = (input) =>{
   //txtViewHatsugenNo onlyTxtViewのときは0を入力してこよう
+  //bar chartにおいて、 hatsugenNo = txtViewHatsugenNo + rectHatsugenNoArr[rectNo]
 
   //hatsugenNoが実際には繰り返されている
 
   let m = input.m;
   const txtViewHatsugenNo = input.txtViewHatsugenNo
-  const allHatsugenIdxArr = input.allHatsugenIdxArr
   const hatsugenNo = input.hatsugenNo
   const bun = input.bun
   const ansTextureChoiceArr = input.ansTextureChoiceArr
   const ansCategoryNoArr = input.ansCategoryNoArr
 
-  console.log(txtViewHatsugenNo + allHatsugenIdxArr[hatsugenNo])
+  //console.log(txtViewHatsugenNo + hatsugenNo[rectNo])
 
-  m.innerHTML += '<font size=' + txtViewFontSize + '>' + (1 + txtViewHatsugenNo + allHatsugenIdxArr[hatsugenNo]) + '' + txtViewClient + ' '
-  for (let bunNo = 0; bunNo < bun[txtViewHatsugenNo + allHatsugenIdxArr[hatsugenNo]].length; bunNo++) {
-    if (bun[txtViewHatsugenNo + allHatsugenIdxArr[hatsugenNo]][bunNo] === '') { continue }
+  m.innerHTML += '<font size=' + txtViewFontSize + '>' + (1 + hatsugenNo) + '' + txtViewClient + ' '
+  for (let bunNo = 0; bunNo < bun[hatsugenNo].length; bunNo++) {
+    if (bun[hatsugenNo][bunNo] === '') { continue }
 
 
 
@@ -29,7 +29,7 @@ const viewAnswerTxt = (input) =>{
     //console.log(ansTextureChoiceArr[ansCategoryNoArr[txtViewHatsugenNo + allHatsugenIdxArr[hatsugenNo]]])
     //console.log(bun[txtViewHatsugenNo + allHatsugenIdxArr[hatsugenNo]][bunNo])
     //console.log(ansTextureChoiceArr[ansCategoryNoArr[txtViewHatsugenNo + allHatsugenIdxArr[hatsugenNo]][bunNo]])
-    m.innerHTML += '<u><font size=' + txtViewFontSize + '><font color=' + ansTextureChoiceArr[ansCategoryNoArr[txtViewHatsugenNo + allHatsugenIdxArr[hatsugenNo]][bunNo]] + '><b>【</b></font>' + bun[txtViewHatsugenNo + allHatsugenIdxArr[hatsugenNo]][bunNo] + '<font color=' + ansTextureChoiceArr[ansCategoryNoArr[txtViewHatsugenNo + allHatsugenIdxArr[hatsugenNo]][bunNo]] + '><b>】</b></font></font></u>'
+    m.innerHTML += '<u><font size=' + txtViewFontSize + '><font color=' + ansTextureChoiceArr[ansCategoryNoArr[hatsugenNo][bunNo]] + '><b>【</b></font>' + bun[hatsugenNo][bunNo] + '<font color=' + ansTextureChoiceArr[ansCategoryNoArr[hatsugenNo][bunNo]] + '><b>】</b></font></font></u>'
 
   }
   m.innerHTML += '<font size=' + txtViewFontSize + '><br><br></font>'
--- src/js/counselorEducation/view/viewQuestionTxt.js
+++ src/js/counselorEducation/view/viewQuestionTxt.js
@@ -3,8 +3,9 @@ import{rodata} from '../rodata'
 const txtViewFontSize=rodata.txtView.fontSize
 const txtViewCounselor=rodata.txtView.counselor
 
-const viewQuestionTxt = (m,allHatsugenNoArr,hatsugenNo,allQuestionHatsugenColorArr,hatsugenArr) =>{
-  m.innerHTML += '<b><u><font size=' + txtViewFontSize + '>' + (1 + allHatsugenNoArr[hatsugenNo]) + '' + txtViewCounselor + ' <font color=' + allQuestionHatsugenColorArr[allHatsugenNoArr[hatsugenNo] / 2] + '>【</font>' + hatsugenArr[allHatsugenNoArr[hatsugenNo]] + '<font color=' + allQuestionHatsugenColorArr[allHatsugenNoArr[hatsugenNo] / 2] + '>】</font></u></b><font size=' + txtViewFontSize + '><br><br>'
+const viewQuestionTxt = (m,rectHatsugenNoArr,hatsugenNo,allQuestionHatsugenColorArr,hatsugenArr) =>{
+  //bar chartにおいて、 hatsugenNo = txtViewHatsugenNo + rectHatsugenNoArr[rectNo]
+  m.innerHTML += '<b><u><font size=' + txtViewFontSize + '>' + (1 + hatsugenNo) + '' + txtViewCounselor + ' <font color=' + allQuestionHatsugenColorArr[hatsugenNo / 2] + '>【</font>' + hatsugenArr[hatsugenNo] + '<font color=' + allQuestionHatsugenColorArr[hatsugenNo / 2] + '>】</font></u></b><font size=' + txtViewFontSize + '><br><br>'
 }
 
 export {viewQuestionTxt}
