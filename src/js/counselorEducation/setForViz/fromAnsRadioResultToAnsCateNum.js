8b7c39534018635c2b538af52a75fb945f9e968d
上辻智也 <uetsuji@viz.media.kyoto-u.ac.jp>
2018-01-10 17:05:15 +0900

    [WIP]謎のズレ発生

--- src/js/counselorEducation/setForViz/fromAnsRadioResultToAnsCateNum.js
+++ src/js/counselorEducation/setForViz/fromAnsRadioResultToAnsCateNum.js
@@ -4,44 +4,46 @@ const fromAnsRadioResultToAnsCateNum = (ansCateNumArr,ansRadioResult,keitaisokai
   console.log(ansBunAnd1stCateArr) //合ってる
   console.log(RGBlist)
 
-  //下記、総当りで検索してたのか。。→同じ文に対して同じ分類が当てはまる。。それはあかんわ。。
+  //下記、総当りで検索してたのか。。→同じ文に対して同じ分類が当てはまる。。それはあかんわ。。→単純にansRadioResultCntを足す方式に変更。
+  let ansRadioResultCnt = 1
 
   let RGBlistCnt = 0// allHatsugenNo=1;allHatsugenNo<keitaisokaiseki.length;allHatsugenNo=allHatsugenNo+2の外
   for (let allHatsugenNo = 1; allHatsugenNo < keitaisokaiseki.length; allHatsugenNo = allHatsugenNo + 2) {
     ansCateNumArr[allHatsugenNo] = []// svgでの描画ではm→hatsugenBunNo
     for (let hatsugenBunNo = 0; hatsugenBunNo < keitaisokaiseki[allHatsugenNo].length; hatsugenBunNo++) {
       ansCateNumArr[allHatsugenNo][hatsugenBunNo] = 0
-      for (let ansRadioResultCnt = 1; ansRadioResultCnt < ansBunAnd1stCateArr.length; ansRadioResultCnt++) {
-        if (bun[allHatsugenNo][hatsugenBunNo] === ansBunAnd1stCateArr[ansRadioResultCnt][0]) {
-          console.log("ansRadioResultCnt %d",ansRadioResultCnt)
+      //for (let ansRadioResultCnt = 1; ansRadioResultCnt < ansBunAnd1stCateArr.length; ansRadioResultCnt++) {
+      //if (bun[allHatsugenNo][hatsugenBunNo] === ansBunAnd1stCateArr[ansRadioResultCnt][0]) {
+      console.log("ansRadioResultCnt %d",ansRadioResultCnt)
 
-          const tmpAnsRadioResult = ansRadioResult[ansRadioResultCnt - 1] //1-5
-          console.log("tmpAnsRadioResult",tmpAnsRadioResult)
+      const tmpAnsRadioResult = ansRadioResult[ansRadioResultCnt - 1] //1-5
+      console.log("tmpAnsRadioResult",tmpAnsRadioResult)
 
-          RGBlist[RGBlistCnt][tmpAnsRadioResult-1] = RGBlist[RGBlistCnt][tmpAnsRadioResult] + 1 //ここ意味わからん
-          ansCateNumArr[allHatsugenNo][hatsugenBunNo] = tmpAnsRadioResult
+      RGBlist[RGBlistCnt][tmpAnsRadioResult-1] = RGBlist[RGBlistCnt][tmpAnsRadioResult] + 1 //ここ意味わからん
+      ansCateNumArr[allHatsugenNo][hatsugenBunNo] = tmpAnsRadioResult
 
-          /*          if (ansRadioResult[ansRadioResultCnt - 1] === 1) {
-                      RGBlist[RGBlistCnt][0] = RGBlist[RGBlistCnt][0] + 1
-                      ansCateNumArr[allHatsugenNo][hatsugenBunNo] = 1
-                    } else if (ansRadioResult[ansRadioResultCnt - 1] === 2) {
-                      RGBlist[RGBlistCnt][1] = RGBlist[RGBlistCnt][1] + 1
-                      ansCateNumArr[allHatsugenNo][hatsugenBunNo] = 2
-                    } else if (ansRadioResult[ansRadioResultCnt - 1] === 3) {
-                      RGBlist[RGBlistCnt][2] = RGBlist[RGBlistCnt][2] + 1
-                      ansCateNumArr[allHatsugenNo][hatsugenBunNo] = 3
-                    } else */
+      /*          if (ansRadioResult[ansRadioResultCnt - 1] === 1) {
+                  RGBlist[RGBlistCnt][0] = RGBlist[RGBlistCnt][0] + 1
+                  ansCateNumArr[allHatsugenNo][hatsugenBunNo] = 1
+                } else if (ansRadioResult[ansRadioResultCnt - 1] === 2) {
+                  RGBlist[RGBlistCnt][1] = RGBlist[RGBlistCnt][1] + 1
+                  ansCateNumArr[allHatsugenNo][hatsugenBunNo] = 2
+                } else if (ansRadioResult[ansRadioResultCnt - 1] === 3) {
+                  RGBlist[RGBlistCnt][2] = RGBlist[RGBlistCnt][2] + 1
+                  ansCateNumArr[allHatsugenNo][hatsugenBunNo] = 3
+                } else */
 
-          //if (ansRadioResult[ansRadioResultCnt - 1] === 4) {
-            console.log(ansBunAnd1stCateArr[ansRadioResultCnt][0])
-          //}
-          /*else if (ansRadioResult[ansRadioResultCnt - 1] === 5) {
-            RGBlist[RGBlistCnt][4] = RGBlist[RGBlistCnt][4] + 1
-            ansCateNumArr[allHatsugenNo][hatsugenBunNo] = 5
-          }*/
+      //if (ansRadioResult[ansRadioResultCnt - 1] === 4) {
+      //console.log(ansBunAnd1stCateArr[ansRadioResultCnt][0])
+      //}
+      /*else if (ansRadioResult[ansRadioResultCnt - 1] === 5) {
+        RGBlist[RGBlistCnt][4] = RGBlist[RGBlistCnt][4] + 1
+        ansCateNumArr[allHatsugenNo][hatsugenBunNo] = 5
+      }*/
 
-        }
-      }
+      //}
+      ansRadioResultCnt++
+      //}
     }
     RGBlistCnt++
   }
