7be20a58efd798313c95b9e151100797806bd395
上辻智也 <uetsuji@viz.media.kyoto-u.ac.jp>
2018-02-02 03:12:33 +0900

    storeQuizResult

--- src/js/charaChart/quiz/readQuiz.js
+++ src/js/charaChart/quiz/readQuiz.js
@@ -8,6 +8,12 @@ import { storeQuizResult } from './storeQuizResult'
 const quizArr=csv2Arr(rodata.quiz.path)
 let quizNo=1
 
+const quizNoCol=0
+const knpCsvCol=1
+const bunArrCsvCol=2
+const isFixedCol=3
+const isColoredCol=4
+const queBunCol=5
 const choiceStartCol=6
 const readQuiz=(quizNo)=>{
 
@@ -56,17 +62,24 @@ const writeQuiz=()=>{
 
 const writeQuestion=(quizNo)=>{
   const quizRow=quizArr[quizNo]
+  const queBun=quizRow[queBunCol]
 
   document.getElementById('msg0').innerHTML+=quizArr[quizNo][1]
+  let target = document.getElementById('msg0')
   target.innerHTML += '<div id="b' + ansCnt + '" style="cursor: pointer"><u>' + (hatsugenIdx + 1) + '(C) ' + bun[hatsugenIdx][bunIdx] + '</u></div><div id="r' + ansCnt + '"><label><input type=radio name="r' + ansCnt + '" value=0>どれにも含まない</label></div><br>'
 
   for(let col=choiceStartCol;col<quizRow.length;col++){
 
-    targetInRow.innerHTML += '<label><input type=radio name="r' + answerNumber + '" value=' + value + '><font color="' + color + '">【</font>「' + answerGroupName + '」に含む<font color="' + color + '">】</font></label>'
+    targetInRow.innerHTML += '<label><input type=radio name="r' + col + '" value=' + col + '>'+queBun+'</label>'
 
   }
 
 
+}
+
+const createAnswerRadioButtonRow = (answerNumber, value, color, answerGroupName) => {
+  const targetInRow = document.getElementById('r' + answerNumber)
+  targetInRow.innerHTML += '<label><input type=radio name="r' + answerNumber + '" value=' + value + '><font color="' + color + '">【</font>「' + answerGroupName + '」に含む<font color="' + color + '">】</font></label>'
 }
 
 
--- src/js/charaChart/quiz/storeQuizResult.js
+++ src/js/charaChart/quiz/storeQuizResult.js
@@ -1,4 +1,17 @@
-const storeQuizResult=(quizResultArr,quizNo,ans,time)=>{
+const radioFullLen=5
+
+const storeQuizResult=(quizResultArr,quizNo,time)=>{
+
+  const answerRadio = document.getElementById('r' + chBoxNo).children
+
+  let ans
+
+  for (let buttonNo = answerRadio.length - radioFullLen, l = answerRadio.length; buttonNo < l; buttonNo++) {
+    if (answerRadio[buttonNo].control.checked) {
+      ans = answerRadio[buttonNo].control.value
+      break
+    }
+  }
 
   quizResultArr.add([quizNo,ans,time])
 }
