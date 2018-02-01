e0fa1001467526b0ec35cdc41ee12c8f26ed2d35
上辻智也 <uetsuji@viz.media.kyoto-u.ac.jp>
2018-02-02 01:23:50 +0900

    quizdefine

--- src/js/charaChart/quiz/quizMain.js
+++ src/js/charaChart/quiz/quizMain.js
@@ -7,6 +7,12 @@ const quizMain=()=>{
 
   const quizArr=csv2Arr(rodata.quiz.path)
   for(let quizNo=1; quizNo<=quizArr.length;quizNo++){
+
+    //ポジネガ問題
+    //何文目〜何文目において，誰と誰との間の言動が最も多いか？
+    //何文目〜何文目において，誰から誰への言動が最も多いか？
+    //何文目〜何文目において，誰と誰との間のpositive/negativeな言動が最も多いか？
+    //何文目〜何文目において，誰から誰へのpositive/negativeな言動が最も多いか？
     selectViz()
     readQuiz
   }
