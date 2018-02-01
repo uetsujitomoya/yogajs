f6e299f9caf323999a9b417b60b4818712b7548a
上辻智也 <uetsuji@viz.media.kyoto-u.ac.jp>
2018-02-02 02:44:53 +0900

    [WIP]writeQuestion

--- src/js/charaChart/quiz/readQuiz.js
+++ src/js/charaChart/quiz/readQuiz.js
@@ -25,7 +25,7 @@ const readQuiz=(quizNo)=>{
   var elapsed_ms = new Date().getTime() - start_ms;
 
   quizNo++
-  document.getElementById('radio_buttons').onchange = () => {
+  document.getElementById('msg0').onchange = () => {
     if(quizNo<=quizArr){
       readQuiz(quizNo)
     }else{
@@ -39,11 +39,24 @@ const readQuiz=(quizNo)=>{
 
 const writeQuiz=()=>{
 
+
+  createTxtBoxInBox(0)
   writeQuestion()
   writeChoice
 }
 
-const writeQuestion=()=>{
+const writeQuestion=(quizNo)=>{
+  const quizRow=quizArr[quizNo]
+
+  document.getElementById('msg0').innerHTML+=quizArr[quizNo][1]
+  target.innerHTML += '<div id="b' + ansCnt + '" style="cursor: pointer"><u>' + (hatsugenIdx + 1) + '(C) ' + bun[hatsugenIdx][bunIdx] + '</u></div><div id="r' + ansCnt + '"><label><input type=radio name="r' + ansCnt + '" value=0>どれにも含まない</label></div><br>'
+
+  for(let col=2;col<quizRow.length;col++){
+
+    targetInRow.innerHTML += '<label><input type=radio name="r' + answerNumber + '" value=' + value + '><font color="' + color + '">【</font>「' + answerGroupName + '」に含む<font color="' + color + '">】</font></label>'
+
+  }
+
 
 }
 
