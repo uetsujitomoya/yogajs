2d6b8c6d73ac9d275a65f23074f58aad18d9c084
上辻智也 <uetsuji@viz.media.kyoto-u.ac.jp>
2018-02-02 01:29:42 +0900

    memo,[WIP]writeQuizAnd Store

--- src/js/charaChart/quiz/quizMain.js
+++ src/js/charaChart/quiz/quizMain.js
@@ -2,12 +2,16 @@ import { readQuiz } from './readQuiz'
 import { csv2Arr } from '../../manageCsv/csv2Arr'
 import { rodata } from '../rodata'
 import { outputQuizResultCsv } from './outputQuizResultCsv'
+import { storeQuizResult } from './storeQuizResult'
 
 const quizMain=()=>{
 
   const quizArr=csv2Arr(rodata.quiz.path)
   for(let quizNo=1; quizNo<=quizArr.length;quizNo++){
 
+    //bunArrCsv
+    //knpCsv
+    //rodata
     //ポジネガ問題
     //何文目〜何文目において，誰と誰との間の言動が最も多いか？
     //何文目〜何文目において，誰から誰への言動が最も多いか？
@@ -15,6 +19,8 @@ const quizMain=()=>{
     //何文目〜何文目において，誰から誰へのpositive/negativeな言動が最も多いか？
     selectViz()
     readQuiz
+    storeQuizResult
+    //1問1問更新シたほうがよくね？
   }
   outputQuizResultCsv
 
