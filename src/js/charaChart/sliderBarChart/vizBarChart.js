968a3a10133707bf0dd0d3f9cdfa1025c79c73fc
上辻智也 <uetsuji@viz.media.kyoto-u.ac.jp>
2018-03-02 13:45:39 +0900

    hotfix: merging slider bar chart

--- src/js/charaChart/sliderBarChart/vizBarChart.js
+++ src/js/charaChart/sliderBarChart/vizBarChart.js
@@ -1,93 +1,9 @@
-import d3 from 'd3'
+
+import * as d3 from 'd3'
+
 import { charaChartRodata } from '../rodata'
 
 const vizBarChart=(strArr,svg,barLenArr,fullW,axisShiftX,chartShiftX)=>{
-
-  //let axisShiftX = 68
-
-  var xScale = d3.scale.linear()
-    .domain([0, d3.sum(barLenArr) / 10])
-    .range([axisShiftX, fullW - axisShiftX])
-
-  let tooltip = d3.select("body").select("#tooltip")
-  let row = 0// graph3の行番号
-  // 階層構造をとるため，g要素を生成する部分とrect要素を生成している部分が連続している．
-  svg.selectAll('g')
-    .data(strArr)
-    .enter()
-    .append('g')
-    .attr('transform', function (d, i) {
-      return 'translate(0,' + (i * 50) + ')'
-    })
-    .selectAll('rect')
-    .data(function (d) { return d })
-    .enter()
-    .append('rect')// 四角追加
-    .attr('x', function (d, i) {
-      var arr = barLenArr
-      // var sum = d3.sum(arr);
-      var subSum = d3.sum(i === 0 ? [] : arr.slice(0, i))
-      return xScale(subSum) / 10 + 10 + chartShiftX
-    })
-    .attr('y', 10)
-    .attr('width', function (d) {
-      // var sum = d3.sum(barLenArr);
-      return xScale(d) / 10
-    })
-    .attr('height', 20)
-    .attr('fill', function (d, i) {
-      //     if ((row === 0 && isFullConversationAnsArr[i] === 0) || (row === 1 && isFullConversationAnsArr[i] === 1)) {
-      /*        if (i + 1 === barTxtArr.length) {
-                row++
-              }*/
-      //return barChartAllHatsugenCArr[i] //質問の場合はtextureを使いたい
-      //一個の関数にして振り分けたい（配列やreturnはまずそう）
-
-      /*if(/*answer){
-        //moyou
-      }else{
-        //questionColor
-      }*/
-
-      switch (i%2) {
-        case 0:
-          return '#a0a0a0'
-          break
-        default:
-          return '#f0f0f0'
-          break
-      }
-      /*        }
-            } else {
-              if (i + 1 === barTxtArr.length) {
-                row++
-              }
-              return barChartBGColor
-            }*/
-    })
-/*    .on("mouseover", function(d,rectNo){
-      if (isFullConversationAnsArr[rectNo] === 0) {
-
-        return tooltip.style("visibility", "visible").text(hatsugen[rectHatsugenNoArr[rectNo]]);
-      }else{
-
-        let msg=""
-        const txtViewHatsugenNo=0
-
-        for (let hatsugenBunNo = 0; hatsugenBunNo < bun[txtViewHatsugenNo + rectHatsugenNoArr[rectNo]].length; hatsugenBunNo++) {
-          if (bun[txtViewHatsugenNo + rectHatsugenNoArr[rectNo]][hatsugenBunNo] === '') { continue }
-
-          msg+= bun[txtViewHatsugenNo + rectHatsugenNoArr[rectNo]][hatsugenBunNo]
-        }
-
-        return tooltip.style("visibility", "visible").text( msg );
-
-      }
-
-
-    })//吹き出し
-    .on("mousemove", function(d){return tooltip.style("top", (event.pageY-20)+"px").style("left",(event.pageX+10)+"px");})
-    .on("mouseout", function(d){return tooltip.style("visibility", "hidden");})*/
 
 }
 
--- src/js/charaChart/sliderBarChart/vizSliderBarChart.js
+++ src/js/charaChart/sliderBarChart/vizSliderBarChart.js
@@ -5,7 +5,6 @@ import { borderBunNoArr } from './borderBunNoArr'
 
 const vizSliderBarChart=()=>{
   //svg
-
   const fullW=charaChartRodata.sliderBarChart.fullW
   let svg = d3.select(charaChartRodata.sliderBarChart.id).append('svg')
     .attr({
@@ -13,7 +12,81 @@ const vizSliderBarChart=()=>{
       height: charaChartRodata.sliderBarChart.h
     })
   //add四角
-  vizBarChart([null,null,null,null,null],svg,borderBunNoArr,fullW,0,0)
+
+  console.log(svg)
+  vizBarChart(["hoge","hoge","hoge","hoge","hoge"],svg,borderBunNoArr,fullW,0,0)
+  const barLenArr=borderBunNoArr
+  const strArr=["8/1","10/2","10/30","1/8","3/18"]
+  const axisShiftX=0
+  const chartShiftX=charaChartRodata.sliderBarChart.chartShiftX
+
+  //let axisShiftX = 68
+  console.log(barLenArr)
+
+  var xScale = d3.scale.linear()
+    .domain([0, d3.sum(barLenArr) / 10])
+    .range([axisShiftX, fullW - axisShiftX])
+
+  let tooltip = d3.select("body").select("#tooltip")
+  // let row = 0// graph3の行番号
+  // 階層構造をとるため，g要素を生成する部分とrect要素を生成している部分が連続している．
+
+  var dataArr = [
+    barLenArr,
+    barLenArr
+  ]
+
+  svg.selectAll('g')
+    .data(dataArr)
+    .enter()
+    .append('g')
+    .attr('transform', function (d, i) {
+      console.log(i)
+      return 'translate(0,' + (i * 50) + ')'
+    })
+    .selectAll('rect')
+    .data(function (d) {
+      console.log(d)
+      return d
+    })
+    .enter()
+    .append('rect')// 四角追加
+    .attr('x', function (d, i) {
+      console.log(i)
+      var arr = barLenArr
+      // var sum = d3.sum(arr);
+      var subSum = d3.sum(i === 0 ? [] : arr.slice(0, i))
+      return xScale(subSum) / 10 + 10 + chartShiftX
+    })
+    .attr('y', 10)
+    .attr('width', function (d) {
+      //console.log(i)
+      // var sum = d3.sum(barLenArr);
+      return xScale(d) / 10
+    })
+    .attr('height', 20)
+    .attr('fill', function (d, i) {
+      console.log(i)
+      switch (i%2) {
+        case 0:
+          return '#a0a0a0'
+          break
+        default:
+          return '#f0f0f0'
+          break
+      }
+
+    })
+    .on("mouseover", function(d,rectNo){
+
+      console.log(strArr[rectNo])
+
+        return tooltip.style("visibility", "visible").text(strArr[rectNo]);
+
+    })//吹き出し
+    .on("mousemove", function(d){return tooltip.style("top", (event.pageY-20)+"px").style("left",(event.pageX+10)+"px");})
+    .on("mouseout", function(d){return tooltip.style("visibility", "hidden");})
+
 
 }
 
